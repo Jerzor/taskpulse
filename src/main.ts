94c76424c2910799bbebbfd02404ba7bddbf90c0
krzysztofjerzyk <krzysztof.jerzyk@merito.pl>
2025-11-03 18:19:32 +0100

    add swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // auto validation DTO, whitelist deletes undefined fields
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('TakePulse API Documentation')
+    .setDescription('TakePulse API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
